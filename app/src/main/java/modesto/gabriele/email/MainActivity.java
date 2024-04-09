33df821403c4749cf6b719251fab4770ca490f07
20221TIIMI0319 <bibiairam1@gmail.com>
2024-04-09 09:01:16 -0300

    commit final

--- app/src/main/java/modesto/gabriele/email/MainActivity.java
+++ app/src/main/java/modesto/gabriele/email/MainActivity.java
@@ -2,7 +2,9 @@ package modesto.gabriele.email;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,19 +30,20 @@ public class MainActivity extends AppCompatActivity {
                 EditText etTexto = (EditText) findViewById(R.id.etTexto);
                 String texto = etTexto.getText().toString();
 
+                //Criando a intencao para navegar para outra app
                 Intent i = new Intent(Intent.ACTION_SENDTO);
-
-                i.setData(Uri.parse("mailto: "));
-
+                //Definindo o tipo de app que o android deverá abrir (no caso email)
+                i.setData(Uri.parse("mailto:"));
+                //Preenchendo os dados que iremos enviar para outra tela
                 String[] emails = new String[] {email};
                 i.putExtra(Intent.EXTRA_EMAIL, emails);
                 i.putExtra(Intent.EXTRA_SUBJECT, assunto);
                 i.putExtra(Intent.EXTRA_TEXT, texto);
-
+                //Caso possua mais de uma opção de app, o usuario podera escolher
                 try {
                     startActivity(Intent.createChooser(i, "Escolha o APP: "));
 
-                }
+                } // caso nao possua nenhum app insta, o programa ira mostrar uma mensagem de erro
                 catch (ActivityNotFoundException e) {
                     Toast.makeText(MainActivity.this, "Não há nenhum app que possa realizar essa operação", Toast.LENGTH_LONG).show();
 
